1c3bda55742d04fc9c8effe39ba8cb0695ec229c
Gabriel-Sila-Souza <gsilvasouza2000@gmail.com>
2020-10-04 12:38:35 -0300

    Home finalizado

--- FSAPP_V1/src/Drawer/Home.js
+++ FSAPP_V1/src/Drawer/Home.js
@@ -2,6 +2,8 @@ import React, {Component, useRef} from 'react';
 import {Text, StyleSheet, View,Image} from 'react-native'
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import {Modalize} from 'react-native-modalize' 
+import {QuadroHorario} from '../components/QuadroHorario'
+import {QuadroDivulgacao} from '../components/QuadroDivulgacao'
 
 export default function Home({ navigation }){
     const modalizeRef= useRef(null);
@@ -39,19 +41,15 @@ export default function Home({ navigation }){
             <View style={{width:138, height: 138, borderRadius: 20, borderWidth: 2, marginTop: 20, alignSelf: "center", backgroundColor: 'white'}}>
                 <Image style={{width:138, height: 138}}/>
             </View>
-            <View style={{marginTop: 29, width: '100%', height: 80}}>
+            <View style={{marginTop: 29, width: '80%', height: 80, alignSelf: "center"}}>
                 <Text style={{alignSelf: "flex-start", color: 'white', marginTop:10 }}> NOME: </Text>
                 <Text style={{alignSelf: "flex-start", color: 'white'}}> RM: </Text>  
                 <Text style={{alignSelf: "flex-end", marginTop: -40, marginRight: 60, color: 'white'}}>TURMA:  </Text>
                 <Text style={{alignSelf: "flex-end", color: 'white'}}>NOME DO CURSO: </Text>
             </View>
 
-            <View style={{width:326, height: 154, borderRadius: 20, borderWidth: 2, marginTop: 20, alignSelf: "center", backgroundColor: 'white'}}>
-
-            </View>
-            <View style={{width:326, height: 81, borderRadius: 20, borderWidth: 2, marginTop: 40, alignSelf: "center", backgroundColor: 'white'}}>
-
-            </View>
+            <QuadroHorario />
+            <QuadroDivulgacao />
 
             <Modalize
             ref={modalizeRef}
